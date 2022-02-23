21c297a0c8f819a0511ca830c1f9b1bff8441db0
Anh Le <lhanhfire1999@gmail.com>
2022-02-24 00:47:41 +0700

    carousel & animation for hero-section

--- app.js
+++ app.js
@@ -3,4 +3,18 @@ $(document).ready(() => {
     $('#hamburger-menu').toggleClass('active');
     $('#nav-menu').toggleClass('active');
   });
+
+  const navText = [
+    "<i class='bx bx-chevron-left'></i>",
+    "<i class='bx bx-chevron-right'></i>",
+  ];
+  // Setting owl  carousel
+  $('#hero-carousel').owlCarousel({
+    items: 1,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: navText,
+    autoplayHoverPause: true,
+  });
 });
