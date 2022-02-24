4f3e8579ac5fe1d22e184e50f7d0750a2fbd9336
Anh Le <lhanhfire1999@gmail.com>
2022-02-24 18:41:35 +0700

    top-movies & responsive for hero-section

--- app.js
+++ app.js
@@ -21,10 +21,21 @@ $(document).ready(() => {
   });
 
   $('#top-movies-slide').owlCarousel({
-    items: 6,
+    items: 2,
     dots: false,
     loop: true,
-    autoPlay: false,
+    autoplay: true,
     autoplayHoverPause: true,
+    responsive: {
+      500: {
+        items: 3,
+      },
+      1280: {
+        items: 4,
+      },
+      1600: {
+        items: 6,
+      },
+    },
   });
 });
